1ba2c184b0bcb07a47b77d906d1dd7abb1f352b0
Ashe David Sterkehus <ashe.goulding@gmail.com>
2016-08-21 07:10:24 +0900

    Added example: tests/uuid/libuuid_perf.cpp

--- examples/tests/uuid/libuuid_perf.cpp
+++ examples/tests/uuid/libuuid_perf.cpp
@@ -1,54 +1,146 @@
 #include <iostream>
-
-#include "UUID/UUID.h"
-#include "uuid/uuid.h"
-
-#include <thread>
 #include <iomanip>
 
-static unsigned long long luuid = 0, lashe = 0;
+#include <thread>
+#include <mutex>
+#include <future>
+#include <condition_variable>
 
-void libuuidRun()
+#include <signal.h>
+
+#include "libashe/helper.h"
+#include "libashe/UUID.h"
+#include "uuid/uuid.h"
+
+static unsigned long long __luuid__ = 0, __lashe__ = 0;
+static bool __flag__ = false;
+static std::promise<void> __prm__[2];
+static std::condition_variable __cv__;
+static std::mutex __mtx__;
+
+static void __startSync(const size_t x) noexcept
+{
+	std::unique_lock<std::mutex> __ul__(::__mtx__);
+
+	::__prm__[x].set_value();
+	::__cv__.wait(__ul__);
+}
+
+static void __libuuidRun() noexcept
 {
 	uuid_t id;
 	char unparsed[37];
-	while(true)
+
+	__startSync(0);
+	while(__flag__)
 	{
 		::uuid_generate_random(id);
 		::uuid_unparse(id, unparsed);
-		++::luuid;
+		++::__luuid__;
 	}
 }
 
-void asheUUIDRun()
+static void __asheUUIDRun() noexcept
 {
-	ashe::UUID::MersenneTwisterEngine theEngine;
-	ashe::UUID id = theEngine();
-	while(true)
+	ashe::UUID id;
+	char unparsed[36];
+
+	__startSync(1);
+	while(__flag__)
 	{
-		id = theEngine();
-		id.toString();
-		++::lashe;
+		id = ashe::uuid::generate();
+		id.string(unparsed);
+		++::__lashe__;
+	}
+}
+
+void __onSignal(const int sn) noexcept
+{
+	switch(sn)
+	{
+	case SIGINT:
+		::signal(SIGINT, SIG_DFL);
+		::__flag__ = false;
+		::__cv__.notify_all();
+		std::cerr << "** Interrupted." << std::endl;
+		break;
+	default: std::abort();
 	}
 }
 
 int main()
 {
-	std::thread uuidTh(libuuidRun);
-	std::thread asheTh(asheUUIDRun);
-
+	std::thread uuidTh;
+	std::thread asheTh;
 	unsigned long long uuid, ashe, ruuid, rashe;
 	char comp;
 	double by;
 
-	while(true)
+	try
 	{
-		uuid = ::luuid;
-		ashe = ::lashe;
-		std::this_thread::sleep_for(std::chrono::seconds(1));
+		static const uint32_t
+			initAbilities[] = {ashe::LAANS_OPENSSL, 0},
+			initFlags[] = {ashe::LAIF_OSCODE, ASHE_HOST_OSCODE, 0};
 
-		ruuid = ::luuid - uuid;
-		rashe = ::lashe - ashe;
+		std::future<void> fut[2];
+
+		ashe::initLibAshe(initAbilities, initFlags);
+
+		// Print out samples.
+		{
+			uuid_t luuid_id;
+			ashe::UUID lashe_id;
+			char unparsed[37];
+
+			::uuid_generate_random(luuid_id);
+			::uuid_unparse(luuid_id, unparsed);
+			std::cerr << "libuuid generates: " << unparsed << std::endl;
+
+			lashe_id = ashe::uuid::generate();
+			lashe_id.string(unparsed);
+			unparsed[36] = 0;
+			std::cerr << "libashe generates: " << unparsed << std::endl;
+		}
+
+		::__flag__ = true;
+		fut[0] = ::__prm__[0].get_future();
+		fut[1] = ::__prm__[1].get_future();
+		uuidTh = std::thread(__libuuidRun);
+		asheTh = std::thread(__asheUUIDRun);
+		fut[0].get();
+		fut[1].get();
+		{
+			std::unique_lock<std::mutex> __ul__(::__mtx__);
+			::__cv__.notify_all();
+		}
+	}
+	catch(ashe::HelperException &e)
+	{
+		std::cerr << e.what() << std::endl;
+		ashe::deinitLibAshe();
+		return 2;
+	}
+	catch(std::system_error &e)
+	{
+		std::cerr << e.what() << std::endl;
+		ashe::deinitLibAshe();
+		return 2;
+	}
+
+	::signal(SIGINT, ::__onSignal);
+	while(::__flag__)
+	{
+		uuid = ::__luuid__;
+		ashe = ::__lashe__;
+		{
+			std::unique_lock<std::mutex> __ul__(::__mtx__);
+			::__cv__.wait_for(__ul__, std::chrono::seconds(1));
+			if(!::__flag__)
+				break;
+		}
+
+		ruuid = ::__luuid__ - uuid;
+		rashe = ::__lashe__ - ashe;
 		if(ruuid > rashe)
 		{
 			comp = '>';
@@ -68,5 +160,9 @@ int main()
 		std::cerr << std::endl;
 	}
 
+	uuidTh.join();
+	asheTh.join();
+
+	ashe::deinitLibAshe();
 	return 0;
 }
