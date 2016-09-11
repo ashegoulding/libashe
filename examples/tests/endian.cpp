e57346d2a1f425dfa9476266e3583ab3a2e602a9
Ashe David Sterkehus <ashe.goulding@gmail.com>
2016-09-11 16:33:01 +0900

    Opti: Endian -- unrolled loop of common sizes.

--- examples/tests/endian.cpp
+++ examples/tests/endian.cpp
@@ -10,14 +10,14 @@
 
 
 static void __printHelp__(std::ostream &s, const char *pname) noexcept;
-
+#define __REINTP_TYPE(T, x) *((T*)x)
 
 int main(const int argc, const char **args)
 {
 	static const std::regex __RE_NUMERAL__(LASHE_REGEX_FORMAT_NUMERAL);
 
 	std::stringstream sb;
-	uint64_t n, tmp;
+	uint64_t n;
 	uint8_t arr[64];
 
 	if(argc < 2)
@@ -37,16 +37,38 @@ int main(const int argc, const char **args)
 	sb >> n;
 
 	std::cout << n << ": 0x" << std::hex << std::noshowbase << std::setw(16) << std::setfill('0') << n << std::endl;
-	tmp = n;
-	ashe::invertEndian_lb__(&tmp, 8);
-	std::cout << "  invertEndian_lb__(): 0x" << std::hex << std::noshowbase << std::setw(16) << std::setfill('0') << tmp << std::endl;
-	tmp = n;
-	ashe::invertEndian_ml__(&tmp, 8);
-	std::cout << "  invertEndian_ml__(): 0x" << std::hex << std::noshowbase << std::setw(16) << std::setfill('0') << tmp << std::endl;
-	tmp = n;
-	ashe::invertEndian_mb__(&tmp, 8);
-	std::cout << "  invertEndian_mb__(): 0x" << std::hex << std::noshowbase << std::setw(16) << std::setfill('0') << tmp << std::endl;
+	// 64bit
+	__REINTP_TYPE(uint64_t, arr) = n;
+	ashe::invertEndian_lb__(arr, 8);
+	std::cout << "  invertEndian_lb__(): 0x" << std::hex << std::noshowbase << std::setw(16) << std::setfill('0') << __REINTP_TYPE(uint64_t, arr) << std::endl;
+	__REINTP_TYPE(uint64_t, arr) = n;
+	ashe::invertEndian_ml__(arr, 8);
+	std::cout << "  invertEndian_ml__(): 0x" << std::hex << std::noshowbase << std::setw(16) << std::setfill('0') << __REINTP_TYPE(uint64_t, arr) << std::endl;
+	__REINTP_TYPE(uint64_t, arr) = n;
+	ashe::invertEndian_mb__(arr, 8);
+	std::cout << "  invertEndian_mb__(): 0x" << std::hex << std::noshowbase << std::setw(16) << std::setfill('0') << __REINTP_TYPE(uint64_t, arr) << std::endl;
+	// 32bit
+	__REINTP_TYPE(uint32_t, arr) = (uint32_t)n;
+	ashe::invertEndian_lb__(arr, 4);
+	std::cout << "  invertEndian_lb__(): 0x" << std::hex << std::noshowbase << std::setw(8) << std::setfill('0') << __REINTP_TYPE(uint32_t, arr) << std::endl;
+	__REINTP_TYPE(uint32_t, arr) = (uint32_t)n;
+	ashe::invertEndian_ml__(arr, 4);
+	std::cout << "  invertEndian_ml__(): 0x" << std::hex << std::noshowbase << std::setw(8) << std::setfill('0') << __REINTP_TYPE(uint32_t, arr) << std::endl;
+	__REINTP_TYPE(uint32_t, arr) = (uint32_t)n;
+	ashe::invertEndian_mb__(arr, 4);
+	std::cout << "  invertEndian_mb__(): 0x" << std::hex << std::noshowbase << std::setw(8) << std::setfill('0') << __REINTP_TYPE(uint32_t, arr) << std::endl;
+	// 16bit
+	__REINTP_TYPE(uint16_t, arr) = (uint16_t)n;
+	ashe::invertEndian_lb__(arr, 2);
+	std::cout << "  invertEndian_lb__(): 0x" << std::hex << std::noshowbase << std::setw(4) << std::setfill('0') << __REINTP_TYPE(uint16_t, arr) << std::endl;
+	__REINTP_TYPE(uint16_t, arr) = (uint16_t)n;
+	ashe::invertEndian_ml__(arr, 2);
+	std::cout << "  invertEndian_ml__(): 0x" << std::hex << std::noshowbase << std::setw(4) << std::setfill('0') << __REINTP_TYPE(uint16_t, arr) << std::endl;
+	__REINTP_TYPE(uint16_t, arr) = (uint16_t)n;
+	ashe::invertEndian_mb__(arr, 2);
+	std::cout << "  invertEndian_mb__(): 0x" << std::hex << std::noshowbase << std::setw(4) << std::setfill('0') << __REINTP_TYPE(uint16_t, arr) << std::endl;
 
+	// Uncommon type tests.
 	::memset(arr, 0, 64);
 	::memcpy(arr, &n, 8);
 	ashe::invertEndian_lb__(arr, 64);
@@ -63,5 +85,7 @@ int main(const int argc, const char **args)
 void __printHelp__(std::ostream &s, const char *pname) noexcept
 {
 	s << "LibAshe Endian test program." << std::endl
-		<< "Usage: " << pname << " <Number>" << std::endl;
+		<< "Usage: " << pname << " <Number>" << std::endl
+		<< std::endl
+		<< "Bug report: ashe.goulding@gmail.com" << std::endl;
 }
--- src/Endian.cpp
+++ src/Endian.cpp
@@ -7,32 +7,23 @@ namespace ashe
 
 static void __dropif_odd__(const size_t x) LASHE_NOEXCEPT
 {
-	switch(x)
-	{
-	case 0:
+	if(0 == x)
 		__die_critical();
-		break;
-	case 2: // short
-	case 4: // int or float
-	case 8: // long or double
-	case 16: // quad
-		break;
-	default:
+	else
 	{
 		/* To understand the basics behind this,
-		* draw the graph of: y = floor(log(x) / log(2))
-		* This formula checks if 'x' is the first value in its horizontal line.
-		* (Where floor(a) == floor(b).)
-		*
-		* No idea on handling overflow or underflow happens here,
-		* which some machine would make.
-		*/
+		 * draw the graph of: y = floor(log(x) / log(2))
+		 * This formula checks if 'x' is the first value in its horizontal line.
+		 * (Where floor(a) == floor(b).)
+		 *
+		 * No idea on handling overflow or underflow happens here,
+		 * which some machine would make.
+		 */
 		const double a = std::log2(x - 1);
 		const double b = std::log2(x);
 
 		if((long long)a == (long long)b)
 			__die_critical();
-	}
 	}
 }
 
@@ -40,54 +31,140 @@ static void __dropif_odd__(const size_t x) LASHE_NOEXCEPT
 
 void invertEndian_lb__(void* buf, const size_t len) LASHE_NOEXCEPT
 {
-	size_t a, b;
-
-	__dropif_odd__(len);
-	a = 0;
-	b = len - 1;
-	while(a < b)
+	switch(len)
 	{
-		std::swap(__INTP_BUF(buf)[a], __INTP_BUF(buf)[b]);
-		++a;
-		--b;
+	case 2: // short
+		std::swap(__INTP_BUF(buf)[0], __INTP_BUF(buf)[1]);
+		break;
+	case 4: // int
+		std::swap(__INTP_BUF(buf)[0], __INTP_BUF(buf)[3]);
+		std::swap(__INTP_BUF(buf)[1], __INTP_BUF(buf)[2]);
+		break;
+	case 8: // long
+		std::swap(__INTP_BUF(buf)[0], __INTP_BUF(buf)[7]);
+		std::swap(__INTP_BUF(buf)[1], __INTP_BUF(buf)[6]);
+		std::swap(__INTP_BUF(buf)[2], __INTP_BUF(buf)[5]);
+		std::swap(__INTP_BUF(buf)[3], __INTP_BUF(buf)[4]);
+		break;
+	case 16: // GUID
+		std::swap(__INTP_BUF(buf)[0], __INTP_BUF(buf)[15]);
+		std::swap(__INTP_BUF(buf)[1], __INTP_BUF(buf)[14]);
+		std::swap(__INTP_BUF(buf)[2], __INTP_BUF(buf)[13]);
+		std::swap(__INTP_BUF(buf)[3], __INTP_BUF(buf)[12]);
+		std::swap(__INTP_BUF(buf)[4], __INTP_BUF(buf)[11]);
+		std::swap(__INTP_BUF(buf)[5], __INTP_BUF(buf)[10]);
+		std::swap(__INTP_BUF(buf)[6], __INTP_BUF(buf)[9]);
+		std::swap(__INTP_BUF(buf)[7], __INTP_BUF(buf)[8]);
+		break;
+	default:
+	{
+		size_t a, b;
+
+		__dropif_odd__(len);
+		a = 0;
+		b = len - 1;
+		while(a < b)
+		{
+			std::swap(__INTP_BUF(buf)[a], __INTP_BUF(buf)[b]);
+			++a;
+			--b;
+		}
+	}
 	}
 }
 
 void invertEndian_ml__(void* buf, const size_t len) LASHE_NOEXCEPT
 {
-	size_t a, b;
-
-	__dropif_odd__(len);
-	a = 0;
-	b = len / 2;
-	while(b < len)
+	switch(len)
 	{
-		std::swap(__INTP_BUF(buf)[a], __INTP_BUF(buf)[b]);
-		++a;
-		++b;
+	case 2: // short
+		std::swap(__INTP_BUF(buf)[0], __INTP_BUF(buf)[1]);
+		break;
+	case 4: // int
+		std::swap(__INTP_BUF(buf)[0], __INTP_BUF(buf)[2]);
+		std::swap(__INTP_BUF(buf)[1], __INTP_BUF(buf)[3]);
+		break;
+	case 8: // long
+		std::swap(__INTP_BUF(buf)[0], __INTP_BUF(buf)[4]);
+		std::swap(__INTP_BUF(buf)[1], __INTP_BUF(buf)[5]);
+		std::swap(__INTP_BUF(buf)[2], __INTP_BUF(buf)[6]);
+		std::swap(__INTP_BUF(buf)[3], __INTP_BUF(buf)[7]);
+		break;
+	case 16: // GUID
+		std::swap(__INTP_BUF(buf)[0], __INTP_BUF(buf)[8]);
+		std::swap(__INTP_BUF(buf)[1], __INTP_BUF(buf)[9]);
+		std::swap(__INTP_BUF(buf)[2], __INTP_BUF(buf)[10]);
+		std::swap(__INTP_BUF(buf)[3], __INTP_BUF(buf)[11]);
+		std::swap(__INTP_BUF(buf)[4], __INTP_BUF(buf)[12]);
+		std::swap(__INTP_BUF(buf)[5], __INTP_BUF(buf)[13]);
+		std::swap(__INTP_BUF(buf)[6], __INTP_BUF(buf)[14]);
+		std::swap(__INTP_BUF(buf)[7], __INTP_BUF(buf)[15]);
+		break;
+	default:
+	{
+		size_t a, b;
+
+		__dropif_odd__(len);
+		a = 0;
+		b = len / 2;
+		while(b < len)
+		{
+			std::swap(__INTP_BUF(buf)[a], __INTP_BUF(buf)[b]);
+			++a;
+			++b;
+		}
+	}
 	}
 }
 
 void invertEndian_mb__(void* buf, const size_t len) LASHE_NOEXCEPT
 {
-	size_t a, b;
+	switch(len)
+	{
+	case 2: // short
+		break;
+	case 4: // int
+		std::swap(__INTP_BUF(buf)[0], __INTP_BUF(buf)[1]);
+		std::swap(__INTP_BUF(buf)[2], __INTP_BUF(buf)[3]);
+		break;
+	case 8: // long
+		std::swap(__INTP_BUF(buf)[0], __INTP_BUF(buf)[3]);
+		std::swap(__INTP_BUF(buf)[1], __INTP_BUF(buf)[2]);
+		std::swap(__INTP_BUF(buf)[4], __INTP_BUF(buf)[7]);
+		std::swap(__INTP_BUF(buf)[5], __INTP_BUF(buf)[6]);
+		break;
+	case 16: // GUID
+		std::swap(__INTP_BUF(buf)[0], __INTP_BUF(buf)[7]);
+		std::swap(__INTP_BUF(buf)[1], __INTP_BUF(buf)[6]);
+		std::swap(__INTP_BUF(buf)[2], __INTP_BUF(buf)[5]);
+		std::swap(__INTP_BUF(buf)[3], __INTP_BUF(buf)[4]);
+		std::swap(__INTP_BUF(buf)[8], __INTP_BUF(buf)[15]);
+		std::swap(__INTP_BUF(buf)[9], __INTP_BUF(buf)[14]);
+		std::swap(__INTP_BUF(buf)[10], __INTP_BUF(buf)[13]);
+		std::swap(__INTP_BUF(buf)[11], __INTP_BUF(buf)[12]);
+		break;
+	default:
+	{
+		size_t a, b;
 
-	__dropif_odd__(len);
-	a = 0;
-	b = len / 2 - 1;
-	while(a < b)
-	{
-		std::swap(__INTP_BUF(buf)[a], __INTP_BUF(buf)[b]);
-		++a;
-		--b;
+		__dropif_odd__(len);
+		a = 0;
+		b = len / 2 - 1;
+		while(a < b)
+		{
+			std::swap(__INTP_BUF(buf)[a], __INTP_BUF(buf)[b]);
+			++a;
+			--b;
+		}
+		a = len / 2;
+		b = len - 1;
+		while(a < b)
+		{
+			std::swap(__INTP_BUF(buf)[a], __INTP_BUF(buf)[b]);
+			++a;
+			--b;
+		}
 	}
-	a = len / 2;
-	b = len - 1;
-	while(a < b)
-	{
-		std::swap(__INTP_BUF(buf)[a], __INTP_BUF(buf)[b]);
-		++a;
-		--b;
 	}
 }
 
